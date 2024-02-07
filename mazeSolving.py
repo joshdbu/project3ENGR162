17faefebb98f4d97dd5c8d43116eca6df5268e53
joshdbu <78238841+joshdbu@users.noreply.github.com>
2024-02-07 02:18:52 +0000

    Pending changes exported from your codespace

--- mazeSolving.py
+++ mazeSolving.py
@@ -1,10 +1,11 @@
 from MapClass import Map
 from DumbMouseClass import DumbMouse
+from SmartMouseClass import SmartMouse
 
 
 testMap = Map(2,0) 
 tom = DumbMouse(testMap)
-#jerry = 
+jerry =  SmartMouse(testMap)
 
 
 numMove = 0
@@ -14,3 +15,10 @@ while tom.yPos < 5:
     numMove += 1
     tom.printChar()
 print('Tom completed the maze in ', numMove, ' moves. His final yPos is ', tom.yPos, ' and his final x position is ', tom.xPos)
+numMove = 0
+
+while jerry.yPos < 5:
+    
+    jerry.move(testMap)
+    numMove += 1
+print('Jerry completed the maze in ', numMove, ' moves. His final yPos is ', tom.yPos, ' and his final x position is ', tom.xPos)
