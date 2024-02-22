642b070f4cb66b15f7cbd4f9f0dfbd1532942a8a
grant-16 <143277383+grant-16@users.noreply.github.com>
2024-02-22 13:03:42 -0500

    Update Obstacle Avoidance.py

--- Obstacle Avoidance.py
+++ Obstacle Avoidance.py
@@ -1,4 +1,23 @@
 import time     # import the time library for the sleep function
 import brickpi3 # import the BrickPi3 drivers
-import grovepi
+import math
+
+from MapClass import Map
+from RobotClass import Robot
+
+careBot = Robot()
+
+radius = 10
+reading = 821 * exp(-0.215 * radius)
+
+careBot.gyroTurn(180, -90)
+# move(radius)
+careBot.gyroTurn(180, 90)
+# move(radius * 2.5)
+careBot.gyroTurn(180, 90)
+# move(radius)
+careBot.gyroTurn(180, -90)
+
+
+
 
