a14ce4240815e48d112d749a4737f919d05de978
grant-16 <143277383+grant-16@users.noreply.github.com>
2024-04-02 18:32:13 -0400

    Update Magnet_Sensor.py

--- Current Robot Code/Magnet_Sensor.py
+++ Current Robot Code/Magnet_Sensor.py
@@ -1,25 +1,20 @@
 from __future__ import print_function 
 import grovepi
+from MPU9250 import MPU9250
+import math
+import time
 
-class IRSensor:
+class MagSensor:
     def __init__(self): 
-       sensor1= 8		# Pin 8 is A8 Port
-       sensor2 = 9		# Pin 9 is A8 Port 
-       grovepi.pinMode(sensor1,"INPUT")
-       grovepi.pinMode(sensor2,"INPUT")
-
+        mpu9250 = MPU9250()
+    
     # print function
-    def IR_PrintValues(self):
-       [x, y] =  self.IR_Read()
-       print ("One = " + x + "\tTwo = " + y)
+    def Mag_PrintValues(self):
+        [xMag, yMag, zMag] = mpu9250.readMagnet() # magnet sensor values
+        print ("X = " + x + "\tY = " + yMag + "\tZ = " + zMag);
 
     #Read Function		
     def IR_Read(self):
-           
-        sensor1= 8		
-        sensor2 = 9	             
-        sensor1_value = grovepi.analogRead(sensor1)
-        sensor2_value = grovepi.analogRead(sensor2)
-                    
-        return [sensor1_value, sensor2_value]
+        [xMag, yMag, zMag] = mpu9250.readMagnet() # magnet sensor values
+        return [xMag, yMag, zMag]
         
