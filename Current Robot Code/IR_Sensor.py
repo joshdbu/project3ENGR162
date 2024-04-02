8b074fb2b497a33b99270c0906300a5653897f8b
grant-16 <143277383+grant-16@users.noreply.github.com>
2024-04-02 19:17:42 -0400

    Average of 10 readings

--- Current Robot Code/IR_Sensor.py
+++ Current Robot Code/IR_Sensor.py
@@ -6,11 +6,12 @@ Created on Thu Feb 29 15:25:13 2024
 """
 from __future__ import print_function 
 import grovepi
+from statistics import mean
 
 class IRSensor:
     def __init__(self): 
-       sensor1= 8		# Pin 8 is A8 Port
-       sensor2 = 9		# Pin 9 is A8 Port 
+       self.sensor1 = 8		# Pin 8 is A8 Port
+       self.sensor2 = 9		# Pin 9 is A8 Port 
        grovepi.pinMode(sensor1,"INPUT")
        grovepi.pinMode(sensor2,"INPUT")
 
@@ -20,13 +21,13 @@ class IRSensor:
        print ("IR reading: " + reading)
 
     #Read Function		
-    def IR_Read(self):
-           
-        sensor1= 8		
-        sensor2 = 9	             
-        sensor1_value = grovepi.analogRead(sensor1)
-        sensor2_value = grovepi.analogRead(sensor2)
-        avgValue = (sensor1_value + sensor2_value) / 2
-                    
-        return avgValue
+    def IR_Read(self): 
+        readList = []
+        for i in range(10):
+            sensor1_value = grovepi.analogRead(self.sensor1)
+            sensor2_value = grovepi.analogRead(self.sensor2)
+            avgValue = (sensor1_value + sensor2_value) / 2   
+            readList.append(avgValue)
+        meanValue = mean(readList)
+        return meanValue
         
