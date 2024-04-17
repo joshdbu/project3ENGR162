b5346d5d7eccc348e10f540a80a1fdcb38e66254
grant-16 <143277383+grant-16@users.noreply.github.com>
2024-04-17 16:02:28 -0400

    added "warm up" readings

--- Current Robot Code/IRClass.py
+++ Current Robot Code/IRClass.py
@@ -1,28 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 29 15:25:13 2024
-
-@author: Elizabeth
-"""
 from __future__ import print_function 
 import grovepi
 from statistics import mean
 
 class IRSensor:
     def __init__(self): 
-       self.sensor1 = 8		# Pin 8 is A8 Port
-       self.sensor2 = 9		# Pin 9 is A8 Port 
-       grovepi.pinMode(sensor1,"INPUT")
-       grovepi.pinMode(sensor2,"INPUT")
+       self.sensor1 = 8		# Pin 8 is A0 Port
+       self.sensor2 = 9		# Pin 9 is A0 Port 
+       grovepi.pinMode(self.sensor1,"INPUT")
+       grovepi.pinMode(self.sensor2,"INPUT")
 
     # print function
     def IR_PrintValues(self):
        reading = self.IR_Read()
-       print ("IR reading: " + reading)
+       print ("IR reading: " + str(reading))
 
     #Read Function		
     def IR_Read(self): 
         readList = []
+        for j in range(10):
+            temp = grovepi.analogRead(self.sensor1)
+            temp = grovepi.analogRead(self.sensor2)
         for i in range(10):
             sensor1_value = grovepi.analogRead(self.sensor1)
             sensor2_value = grovepi.analogRead(self.sensor2)
@@ -30,4 +27,3 @@ class IRSensor:
             readList.append(avgValue)
         meanValue = mean(readList)
         return meanValue
-        
