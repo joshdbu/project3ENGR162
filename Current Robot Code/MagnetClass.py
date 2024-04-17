370398b703662ecfba16d774a7c38ad8883bdf4d
grant-16 <143277383+grant-16@users.noreply.github.com>
2024-04-17 16:09:31 -0400

    syntax fixes + added magnitude calculation

--- Current Robot Code/MagnetClass.py
+++ Current Robot Code/MagnetClass.py
@@ -1,20 +1,19 @@
 from __future__ import print_function 
-import grovepi
 from MPU9250 import MPU9250
 import math
-import time
 from statistics import mean
+
+mpu9250 = MPU9250()
 
 class Magnet_Sensor:
     def __init__(self): 
-        mpu9250 = MPU9250()
         self.baseline = [0,0]
         self.baseline = self.calibrate()
         
     # print function
     def Mag_PrintValues(self):
-        [xMag, yMag, zMag] = self.Mag_Read()
-        print ("X = " + x + "\tY = " + yMag + "\tZ = " + zMag);
+        [xMag, yMag, zMag, magnitude] = self.Mag_Read()
+        print ("X = " + xMag + "\tY = " + yMag + "\tZ = " + zMag);
 
     #Read Function		
     def Mag_Read(self):
@@ -22,16 +21,20 @@ class Magnet_Sensor:
         yList = []
         zList = []
         for i in range(10):
-            [xMag, yMag, zMag] = mpu9250.readMagnet() # magnet sensor values
+            mag = mpu9250.readMagnet() # magnet sensor values
+            xMag = mag['x']
+            yMag = mag['y']
+            zMag = mag['z']
             xList.append(xMag - self.baseline[0])
             yList.append(yMag - self.baseline[1])
             zList.append(zMag)
         xAvg = mean(xList)
         yAvg = mean(yList)
         zAvg = mean(zList)
-        return [xAvg, yAvg, zAvg]
+        magnitude = math.sqrt(xAvg ** 2 + yAvg ** 2)
+        return [xAvg, yAvg, zAvg, magnitude]
 
     def calibrate(self):
-        [xMag, yMag, zMag] = self.Mag_Read()
+        [xMag, yMag, zMag, magnitude] = self.Mag_Read()
         return[xMag, yMag]
         
