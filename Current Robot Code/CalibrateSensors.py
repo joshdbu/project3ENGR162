89ce0c2149321bab2363f92e7c9053886bd9e7f0
grant-16 <143277383+grant-16@users.noreply.github.com>
2024-04-21 12:56:09 -0400

    I really hope I didn't mess anything up this time

--- Current Robot Code/CalibrateSensors.py
+++ Current Robot Code/CalibrateSensors.py
@@ -4,15 +4,16 @@ from MagnetClass import Magnet_Sensor
 magnet = Magnet_Sensor()
 ir = IRSensor()
 
-temp = input("Press enter to take reading.")
 temp = ir.IR_Read()
 temp = magnet.Mag_Read()
 
+temp = input("Press enter to take IR reading.")
 irRead = ir.IR_Read()
+temp = input("Press enter to take magnet reading.")
 magRead = magnet.Mag_Read()
 
 irThresh = irRead * 0.8
-magThresh = magRead[3] * 0.8
+magThresh = round(magRead[3] * 0.8, 1)
 
 print(f"IR raw: {irRead}   Suggested threshold: {irThresh}")
-print(f"Magnet raw: {magRead[3]}   Suggested threshold: {magThresh}")
+print(f"Magnet raw: {round(magRead[3], 1)}   Suggested threshold: {magThresh}")
