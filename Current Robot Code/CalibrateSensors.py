29e4b0da57965fde7065e420b5fa79a98d6b103f
grant-16 <143277383+grant-16@users.noreply.github.com>
2024-04-21 12:28:37 -0400

    Add files via upload

--- Current Robot Code/CalibrateSensors.py
+++ Current Robot Code/CalibrateSensors.py
@@ -4,5 +4,14 @@ from MagnetClass import Magnet_Sensor
 magnet = Magnet_Sensor()
 ir = IRSensor()
 
-ir.IR_PrintValues()
-magnet.Mag_PrintValues()
+temp = ir.IR_Read()
+temp = magnet.Mag_Read()
+
+irRead = ir.IR_Read()
+magRead = magnet.Mag_Read()
+
+irThresh = irRead * 0.8
+magThresh = irRead * 0.8
+
+print(f"IR raw: {irRead}   Suggested threshold: {irThresh}")
+print(f"Magnet raw: {magRead}   Suggested threshold: {magThresh}")
